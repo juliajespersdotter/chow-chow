88edf37258e6ca9690e9be40c3bf63f96191733b
Julia Jespersdotter <julia.jespersdotter@gmail.com>
2022-09-19 13:19:36 +0200

    current location.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,12 @@
 import { useRef, useState, useEffect } from 'react'
+import Button from 'react-bootstrap/Button'
 
-const Map = ({ center, style, zoom }) => {
+const Map = ({ style, center, zoom }) => {
 	const ref = useRef(null)
 	const [map, setMap] = useState()
+	// const [center, setCenter] = useState({})
+
+	let infoWindow = new google.maps.InfoWindow()
 
 	useEffect(() => {
 		if (ref.current && !map) {
@@ -16,6 +20,24 @@ const Map = ({ center, style, zoom }) => {
 		}
 	}, [ref, map])
 
+	const getCurrentLocation = () => {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(position => {
+				const pos = {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude,
+				}
+				infoWindow.setPosition(pos)
+				infoWindow.setContent('Location found.')
+				infoWindow.open(map)
+				map.setCenter(pos)
+			})
+		} else {
+			// Browser doesn't support Geolocation
+			handleLocationError(false, infoWindow, map.getCenter())
+		}
+	}
+
 	return (
 		<>
 			<div ref={ref} style={style} center={center} zoom={zoom}>
@@ -26,6 +48,9 @@ const Map = ({ center, style, zoom }) => {
 					}
 				})} */}
 			</div>
+			<Button onClick={getCurrentLocation}>
+				Pan to current Location
+			</Button>
 		</>
 	)
 }

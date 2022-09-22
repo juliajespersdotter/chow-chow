cb5a9252fd6deb3d054481748f40952c546b760f
johan-rain <johan@ranestam.se>
2022-09-22 13:31:16 +0200

    trying to get firebase data

--- src/components/FilterForm.jsx
+++ src/components/FilterForm.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import { collection, query, orderBy } from 'firebase/firestore'
+import { useFirestoreQueryData } from '@react-query-firebase/firestore'
+import { db } from '../firebase'
 import Fooddata from './data'
 import Card from 'react-bootstrap/Card'
 
@@ -6,8 +9,19 @@ const FilterForm = () => {
 
     const [menu, setMenu] = useState(Fooddata);
 
+	const foodplaceRef = collection(db, 'foodplaces')
+    const queryRef = query(foodplaceRef, orderBy('name'))
+	const { data: foodplaces } = useFirestoreQueryData(
+		['foodplaces'],
+		queryRef,
+		{
+			idField: 'id',
+			subscribe: true,
+		}
+	)
+
     const filteritems = (curitems) => {
-        const updateitems = Fooddata.filter((cur) => {
+        const updateitems = foodplaces.filter((cur) => {
             return cur.category === curitems;
         })
         setMenu(updateitems);
@@ -15,14 +29,14 @@ const FilterForm = () => {
 
     return (
         <>
-            <section className='iteam_section mt-2 container'>
+            <section className='mt-2'>
                 <h2 className='text-center mb-2' style={{ fontWeight: 400 }}>Search Your Food</h2>
 
                 <div className="btn-container d-flex justify-content-around mt-2">
-                    <button className="btn btn-danger" onClick={() => filteritems("chinese")}>Chinese</button>
+                    <button className="btn btn-primary" onClick={() => filteritems("chinese")}>Chinese</button>
                     <button className="btn btn-primary" onClick={() => filteritems("japanese")}>Japanese</button>
-                    <button className="btn btn-success" onClick={() => filteritems("pizza")}>Pizza</button>
-                    <button className="btn btn-danger" onClick={() => filteritems("coffee")}>Coffee</button>
+                    <button className="btn btn-primary" onClick={() => filteritems("pizza")}>Pizza</button>
+                    <button className="btn btn-primary" onClick={() => filteritems("coffee")}>Coffee</button>
                     <button className="btn btn-primary" onClick={() => setMenu(Fooddata)}>All</button>
                 </div>
 

efc963fbede4b543a03b28cbfc76f0e995e5e399
johan-rain <johan@ranestam.se>
2022-09-26 13:15:05 +0200

    fixed styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,9 +8,7 @@ const HomePage = () => {
 			fluid
 			className='home d-flex justify-content-center align-items-center vh-75 m-0 p-0'
 		>
-			<div className="logo">
-				<Image fluid className='w-25 p-3' src='images/logo.png' />
-			</div>
+			<Image fluid className='logo' src='images/logo.png' />
 		</Container>
 )
 }

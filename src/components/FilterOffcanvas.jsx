e82ccca11dde45b4de528503c1a434dfcb9ee090
johan-rain <johan@ranestam.se>
2022-10-01 16:59:59 +0200

    updated parameters to be consistent with firebase, added Offcanvas component for mappage

---
+++ src/components/FilterOffcanvas.jsx
@@ -0,0 +1,16 @@
+import { useState, useMemo } from 'react'
+import  Offcanvas  from 'react-bootstrap/Offcanvas'
+import Button from 'react-bootstrap/Button'
+import  ListGroup  from 'react-bootstrap/ListGroup'
+import useGetQueryFoodplaces from '../hooks/useGetQueryFoodplaces'
+
+const FilterOffcanvas = ({}) => {
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default FilterOffcanvas
--- src/hooks/useGetQueryFoodplaces.js
+++ src/hooks/useGetQueryFoodplaces.js
@@ -14,33 +14,33 @@ const useGetQueryFoodplaces = (queryLimits) => {
     // variable for keeping track of the diffrent constraints
     let queryRef
 
-    if (queryLimits.townWhere) {
-        // Query to use if neither type or supply has been restricted
-        if (queryLimits.typeWhere === 'All' && queryLimits.supplyWhere === 'All') {
-            queryRef = query(collectionRef, where('town', '==', queryLimits.townWhere), orderBy('name', queryLimits.nameOrder))
-        // Query to use if supply has been restricted
+    if (queryLimits.cityWhere) {
+        // Query to use if neither type or meals has been restricted
+        if (queryLimits.typeWhere === 'All' && queryLimits.mealsWhere === 'All') {
+            queryRef = query(collectionRef, where('city', '==', queryLimits.cityWhere), orderBy('name', queryLimits.nameOrder))
+        // Query to use if meals has been restricted
         } else if (queryLimits.typeWhere === 'All') {
-            queryRef = query(collectionRef, where('supply', '==', queryLimits.supplyWhere), where('town', '==', queryLimits.townWhere), orderBy('name', queryLimits.nameOrder))
+            queryRef = query(collectionRef, where('meals', '==', queryLimits.mealsWhere), where('city', '==', queryLimits.cityWhere), orderBy('name', queryLimits.nameOrder))
         // Query to use if type has been restricted
-        } else if (queryLimits.supplyWhere === 'All') {
-            queryRef = query(collectionRef, where('type', '==', queryLimits.typeWhere), where('town', '==', queryLimits.townWhere), orderBy('name', queryLimits.nameOrder))
-        // Query to use if both type and supply has been restricted
+        } else if (queryLimits.mealsWhere === 'All') {
+            queryRef = query(collectionRef, where('type', '==', queryLimits.typeWhere), where('city', '==', queryLimits.cityWhere), orderBy('name', queryLimits.nameOrder))
+        // Query to use if both type and meals has been restricted
         } else {
-            queryRef = query(collectionRef, where('type', '==', queryLimits.typeWhere), where('supply', '==', queryLimits.supplyWhere), where('town', '==', queryLimits.townWhere), orderBy('name', queryLimits.nameOrder))
+            queryRef = query(collectionRef, where('type', '==', queryLimits.typeWhere), where('meals', '==', queryLimits.mealsWhere), where('city', '==', queryLimits.cityWhere), orderBy('name', queryLimits.nameOrder))
         }
     } else {
-        // Query to use if neither type or supply has been restricted
-        if (queryLimits.typeWhere === 'All' && queryLimits.supplyWhere === 'All') {
+        // Query to use if neither type or meals has been restricted
+        if (queryLimits.typeWhere === 'All' && queryLimits.mealsWhere === 'All') {
             queryRef = query(collectionRef, orderBy('name', queryLimits.nameOrder))
-        // Query to use if supply has been restricted
+        // Query to use if meals has been restricted
         } else if (queryLimits.typeWhere === 'All') {
-            queryRef = query(collectionRef, where('supply', '==', queryLimits.supplyWhere), orderBy('name', queryLimits.nameOrder))
+            queryRef = query(collectionRef, where('meals', '==', queryLimits.mealsWhere), orderBy('name', queryLimits.nameOrder))
         // Query to use if type has been restricted
-        } else if (queryLimits.supplyWhere === 'All') {
+        } else if (queryLimits.mealsWhere === 'All') {
             queryRef = query(collectionRef, where('type', '==', queryLimits.typeWhere), orderBy('name', queryLimits.nameOrder))
-        // Query to use if both type and supply has been restricted
+        // Query to use if both type and meals has been restricted
         } else {
-            queryRef = query(collectionRef, where('type', '==', queryLimits.typeWhere), where('supply', '==', queryLimits.supplyWhere), orderBy('name', queryLimits.nameOrder))
+            queryRef = query(collectionRef, where('type', '==', queryLimits.typeWhere), where('meals', '==', queryLimits.mealsWhere), orderBy('name', queryLimits.nameOrder))
         }
     }
 

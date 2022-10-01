c4ae49c0b39c22f28f131e55f2cd9ebe1372dbf9
johan-rain <johan@ranestam.se>
2022-10-01 18:56:01 +0200

    started using Offcanvas to display filtered restaurants with queryLimits

--- src/components/FilterOffcanvas.jsx
+++ src/components/FilterOffcanvas.jsx
@@ -6,9 +6,29 @@ import useGetQueryFoodplaces from '../hooks/useGetQueryFoodplaces'
 
 const FilterOffcanvas = ({}) => {
 
+    const { data, loading } = useGetQueryFoodplaces(queryLimits)
+
+    const handleShow = () => setShow(true)
+    const handleClose = () => setShow(false)
+    
+
     return (
         <>
+            <Button  onClick={handleShow} className="outline-primary">
+                Filter Restaurants
+            </Button>
 
+            <Offcanvas show={show} onHide={handleClose} className='offcanvas-bg'>
+
+            <Offcanvas.Header closeButton>
+                <Offcanvas.Title className='h-text-color-dark'>Filter through restaurants</Offcanvas.Title>
+            </Offcanvas.Header>
+
+            <Offcanvas.body>
+
+            </Offcanvas.body>
+            
+            </Offcanvas>
         </>
     )
 }

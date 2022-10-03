95d7eb17751f5a0d9d42b4fa5808114935bb5a58
johan-rain <johan@ranestam.se>
2022-10-03 13:48:45 +0200

    merge

--- src/hooks/useFilterFoodplaces.js
+++ src/hooks/useFilterFoodplaces.js
@@ -1,18 +1,9 @@
 import { useFirestoreQueryData } from '@react-query-firebase/firestore'
-<<<<<<< HEAD
-import { collection, query, orderBy, where } from 'firebase/firestore'
-import { db } from '../firebase'
-
-const useFilterFoodplaces = (options = {}) => {
-	// const { currentUser } = useAuthContext()
-	console.log(options)
-=======
 import { collection, query, where, orderBy } from 'firebase/firestore'
 import { db } from '../firebase'
 
 const useFoodplaces = () => {
 	// const { currentUser } = useAuthContext()
->>>>>>> main
 
 	// create ref to collection 'foodplaces'
 	const collectionRef = collection(db, 'foodplaces')
@@ -22,13 +13,7 @@ const useFoodplaces = () => {
 	const queryKey = ['foodplaces']
 
 	// create query for collectionRef, order result by name
-<<<<<<< HEAD
-	const queryRef = options.fetchUnApproved
-		? query(collectionRef, where('approved', '==', false))
-		: query(collectionRef)
-=======
 	const queryRef = query(collectionRef, orderBy('name'))
->>>>>>> main
 
 	// run query
 	const { data: foodplaces, isLoading } = useFirestoreQueryData(
@@ -43,8 +28,4 @@ const useFoodplaces = () => {
 	return { foodplaces, isLoading }
 }
 
-<<<<<<< HEAD
-export default useFilterFoodplaces
-=======
 export default useFoodplaces
->>>>>>> main

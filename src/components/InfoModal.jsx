e31bca785f7c738b75e162a6dba7acf0381b983e
Tobias Silfverberg <tobiassilfverberg@gmail.com>
2022-10-03 09:51:55 +0200

    Show link to Google Maps with directions in OffCanvas.

--- src/components/InfoModal.jsx
+++ src/components/InfoModal.jsx
@@ -1,6 +1,9 @@
 import Offcanvas from 'react-bootstrap/Offcanvas'
+import useDirections from '../hooks/useDirections'
 
 const InfoModal = ({ data, show, onClick }) => {
+	const { url } = useDirections()
+
 	return (
 		<Offcanvas
 			show={show}
@@ -13,6 +16,8 @@ const InfoModal = ({ data, show, onClick }) => {
 			</Offcanvas.Header>
 			<Offcanvas.Body>
 				<h6> {data.description} </h6>
+				Show directions on Google Maps:
+				<a href={url(data.name)} target='_blank'>Click here</a>
 			</Offcanvas.Body>
 			{/* <Modal.Footer>
 				<Button onClick={props.onHide}>Close</Button>

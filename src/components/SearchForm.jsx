c9ea2e00545887c6aaa989f239c3c130efb5342f
johan-rain <johan@ranestam.se>
2022-10-06 11:34:29 +0200

    fixed scrollbar on map, mobile styles, and final searchform

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,9 +1,8 @@
 import { useRef } from 'react'
-import Form from 'react-bootstrap/Form'
 import { useState } from "react";
 import useGetCollection from "../hooks/useGetCollection";
 
-const SearchForm = ({ onSubmit, className }) => {
+const SearchForm = ({ onSubmit }) => {
 	const cityRef = useRef()
 
 	const [searchInput, setSearchInput] = useState("");
@@ -38,11 +37,12 @@ const SearchForm = ({ onSubmit, className }) => {
 		<>
 			<form
             onSubmit={handleSearch}
-            className={`flex justify-center items-center gap-2`}
+            className="search-box"
         	>
 
 				<div className="input-group">
-					<input
+					<input 
+						className="search-input"
 						type="text"
 						placeholder={"Search restaurant"}
 						onChange={(e) => setSearchInput(e.target.value)}
@@ -52,13 +52,13 @@ const SearchForm = ({ onSubmit, className }) => {
 				</div>
 
 				{searchInput.length > 0 && filteredFoodplaces.length > 0 && (
-					<ul className="list-search">
+					<ul className="list-search py-2 px-2">
 						{filteredFoodplaces?.map((foodplace) => (
 							<li
 								key={foodplace.id}
-								className="list-search-item"
+								className="list-search-item mb-2"
 								onClick={() =>
-									setSearchInput(`${foodplace.streetaddress}, ${foodplace.city}`)
+									setSearchInput(`${foodplace.name}, ${foodplace.city}`)
 								}
 							>
 								{foodplace.name}, {foodplace.city}
@@ -67,105 +67,8 @@ const SearchForm = ({ onSubmit, className }) => {
 					</ul>
 				)}
         	</form>
-			{/* <div style={{ 
-				width: 400, 
-				marginTop: 20,
-				marginBottom: 20,
-				}}>
-				<ReactSearchAutocomplete className="searchbar"
-					items={items}
-					maxResults={4}
-					fuseOptions={{ keys: ["city"] }}
-					onSearch={handleOnSearch}
-					onHover={handleOnHover}
-					onSelect={handleOnSelect}
-					onFocus={handleOnFocus}
-					formatResult={formatResult}
-					onSubmit={handleSubmit}
-					ref={cityRef}
-					styling={{ zIndex: 4 }}
-					autoFocus
-				/>
-			</div> */}
-
-			{/* <Form className='p-1' onSubmit={handleSearch}>
-				<Form.Group controlId='city' className='mb-3'>
-					<Form.Control ref={cityRef} type='text' placeholder="Search by city"/>
-				</Form.Group>
-			</Form>	 */}
 		</>
 	)
 }
 
 export default SearchForm
-
-// import { useEffect, useState } from "react";
-// import useGetCollection from "../hooks/useGetCollection";
-
-
-// const SearchForm = ({ className }) => {
-
-//     const [searchInput, setSearchInput] = useState("");
-
-//     const { data: foodplaces } = useGetCollection("foodplaces");
-
-//     const filteredFoodplaces =
-//         searchInput === ""
-//             ? foodplaces
-//             : foodplaces.filter((foodplace) => {
-//                     return (
-//                         foodplace.city.toLowerCase().includes(searchInput.toLowerCase()) ||
-//                         foodplace.name.toLowerCase().includes(searchInput.toLowerCase())
-//                     );
-//               });
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-
-//         if (!searchInput.length) {
-//             return;
-//         }
-
-//         handleSearch(searchInput);
-//         setSearchInput("");
-//     };
-
-//     return (
-//         <form
-//             onSubmit={handleSubmit}
-//             className={`flex justify-center items-center gap-2 ${className}`}
-//         >
-//             <div className="input-group">
-//                 <input
-//                     type="text"
-//                     placeholder={"Sök..."}
-//                     onChange={(e) => setSearchInput(e.target.value)}
-//                     value={searchInput}
-//                     className="input input-sm input-bordered w-full"
-//                 />
-
-//                 <button className="btn btn-sm btn-square">
-//                     <p>search</p>
-//                 </button>
-//             </div>
-
-//             {searchInput.length > 0 && filteredFoodplaces.length > 0 && (
-//                 <ul className="list-search">
-//                     {filteredFoodplaces?.map((foodplace) => (
-//                         <li
-//                             key={foodplace.id}
-//                             className="cursor-pointer hover:bg-base-300 p-2"
-//                             onClick={() =>
-//                                 setSearchInput(`${foodplace.streetaddress}, ${foodplace.city}`)
-//                             }
-//                         >
-//                             {foodplace.name}, {foodplace.city}
-//                         </li>
-//                     ))}
-//                 </ul>
-//             )}
-//         </form>
-//     );
-// };
-
-// export default SearchForm;
